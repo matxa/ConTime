6f085ff5d7274fedd0cee65c8ba9d2192b612454
matxa <42968545+matxa@users.noreply.github.com>
2020-10-16 21:46:26 -0400

    fix backend and frontend

--- FrontEnd/static/JS/dashboard.js
+++ FrontEnd/static/JS/dashboard.js
@@ -7,38 +7,74 @@ else {
     $(".add_employee").val("ADD employee");
 }
 
+
+if ($(window).width() > 650) {
+    
+    $(".form_add").hide();
+    $(".form_hide").hide();
+}
+else {
+    $(".add_employees").hide();
+
+    $(".form_add").show();
+}
+
+$(".form_hide").hide();
+
+
 /* Run script only after page is done loading */
 $(document).ready(() => {
 
     $(window).resize(() => {
+
+        $(".form_add").css("background-color", "#67d380");
+        $(".form_add").text("ADD a new employee");
+
         if ($(window).width() < 800) {
             $(".add_employee").val("ADD");
         }
         else {
             $(".add_employee").val("ADD employee");
         }
+
+        if ($(window).width() > 650) {
+    
+            $(".form_add").css("display", "none");
+            $(".add_employees").show();
+        }
+        else {
+            $(".add_employees").hide();
+        
+            $(".form_add").show();
+            $(".form_hide").hide();
+        }
+
     });
 
-    $(".check_for_del").css("display", "none");
+
+    $(".form_add").click(() => {
+        $(".form_hide").show()
+        $(".form_add").hide()
+        $(".add_employees").show();
+    });
+
+    $(".form_hide").click(() => {
+        $(".form_hide").hide()
+        $(".form_add").show()
+        $(".add_employees").hide();
+    });
+
+
+    $(".check_for_del").hide();
 
     $(".del").click(function() {
-        $(this).closest(".on_employee").find(".cardBtn").css("display", "none");
-        $(this).closest(".on_employee").find(".check_for_del").css({
-            "display": "flex",
-            "align-items": "center",
-            "justify-content": "center",
-            "flex-direction": "column",
-        });
+        $(this).closest(".on_employee").find(".cardBtn").hide();
+        $(this).closest(".on_employee").find(".check_for_del").show();
     });
 
     $(".no").click(function() {
-        $(this).closest(".on_employee").find(".check_for_del").css("display", "none")
-        $(this).closest(".on_employee").find(".cardBtn").css({
-            "display": "flex",
-            "align-items": "center",
-            "justify-content": "space-between",
-            "flex-direction": "row",
-        });
+        $(this).closest(".on_employee").find(".check_for_del").hide();
+        $(this).closest(".on_employee").find(".cardBtn").show();
     });
 });
 
